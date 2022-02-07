fe45b0e8a9f1f10dc7f16f6cfc90969efdbaf536
Carlos lobo <carloslobo@wposs.com>
2022-02-07 16:07:56 -0500

    ♻️ REFACTOR: Se agrega paleta de colores en tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#2F80ED',
+        'gray-1':'#333333',
+        'gray-2': '#4F4F4F',
+        'gray-3': '#828282',
+        'gray-light-1': '#C4C4C4',
+        'gray-light-2': '#E0E0E0',
       }
     },
   },
