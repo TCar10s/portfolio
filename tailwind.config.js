7b9e691e2f7dd946f05f44a4a2d5cacc50c99e3c
Carlos lobo <carloslobo@wposs.com>
2022-02-10 17:16:05 -0500

    🛠️ FIX: Ajuste en dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#2F80ED',
-        'gray-1':'#333333',
-        'gray-2': '#4F4F4F',
-        'gray-3': '#828282',
-        'gray-light-1': '#C4C4C4',
-        'gray-light-2': '#E0E0E0',
-        'card-primary': '#1e293b',
+        primary: "#2F80ED",
+        "gray-1": "#333333",
+        "gray-2": "#4F4F4F",
+        "gray-3": "#828282",
+        "gray-light-1": "#C4C4C4",
+        "gray-light-2": "#E0E0E0",
+        "card-primary": "#1e293b",
+        "body-dark": "#0f172a",
+        "body-light": "#F2F2F2",
       },
       gridTemplateColumns: {
-        'blog': '30% 70%',
-      }
+        blog: "30% 70%",
+      },
     },
   },
   plugins: [],
