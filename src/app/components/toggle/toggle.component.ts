4682f624d99e489abcb29db7594380a0202a9ec8
Carlos lobo <carlitosami47@gmail.com>
2022-02-12 11:56:58 -0500

    ➕ ADD: Implementación del Dark Mode

--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -1,37 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-toggle',
   template: `
     <div
-      class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in"
+      class="relative inline-block w-12 mr-2 align-middle select-none transition duration-200 ease-in"
     >
       <input
+        #toggleInput
         type="checkbox"
         name="toggle"
         id="toggle"
-        class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+        class="toggle"
+        (change)="onChange()"
       />
       <label
         for="toggle"
-        class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+        class="toggle-label block h-8 -ml-1 -mt-1 rounded-full bg-blue-50 cursor-pointer"
       ></label>
     </div>
   `,
   styles: [
     `
+      .toggle {
+        @apply bg-yellow-300 border-yellow-500 mr-1 focus:ring-transparent
+               toggle-checkbox absolute block w-6 h-6 rounded-full border-2
+               appearance-none cursor-pointer;
+      }
+
       .toggle-checkbox:checked {
-        @apply right-0 border-primary;
         right: 0;
+        border: none;
+        background-color: rgb(129 124 214);
+        background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' className='h-10 w-10' fill='%23FFF' viewBox='0 0 24 24' stroke='rgb(129 124 214)' > <path strokeLinecap='round' strokeLinejoin='round' d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z' /> </svg>");
       }
       .toggle-checkbox:checked + .toggle-label {
-        @apply bg-green-400 bg-primary;
+        background-color: rgb(76 73 188);
       }
     `,
   ],
 })
-export class ToggleComponent implements OnInit {
+export class ToggleComponent implements OnInit, AfterViewInit {
+  @ViewChild('toggleInput') toggle!: ElementRef<HTMLInputElement>;
+  html: HTMLElement = document.documentElement;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const theme = localStorage.theme;
+
+    theme
+      ? this.html.classList.add('dark')
+      : this.html.classList.remove('dark');
+  }
+
+  ngAfterViewInit() {
+    if (localStorage.theme) {
+      this.toggle.nativeElement.checked = true;
+    }
+  }
+
+  onChange() {
+    const theme = localStorage.theme;
+
+    if (theme) {
+      this.html.classList.remove('dark');
+      localStorage.removeItem('theme');
+    } else {
+      this.html.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
 }
