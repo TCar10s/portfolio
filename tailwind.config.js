f2fe91e9df969b7b066fc3fa4fb9e3415c95ba8b
Carlos lobo <carlitosami47@gmail.com>
2022-03-13 11:44:41 -0500

    ⚒️ FIX: Ajuste global de dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
         "gray-3": "#828282",
         "gray-light-1": "#C4C4C4",
         "gray-light-2": "#E0E0E0",
-        "card-primary": "#1e293b",
-        "body-dark": "#0f172a",
+        "card-primary": "#1E1E1E",
+        "body-dark": "#121212",
         "body-light": "#F2F2F2",
       },
       gridTemplateColumns: {
